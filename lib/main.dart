37affd120baecec48eaeb917a8b83440568c4947
KojoBoakye <kojoboakyejnr@outlook.com>
2025-08-03 12:34:37 +0000

    Initial app structure without splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quizzia_splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +8,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashScreen());
+    return MaterialApp();
   }
 }
