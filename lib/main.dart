0a5182b84eb50168aae12119ce64ab53fda0d7b2
KojoBoakye <kojoboakyejnr@outlook.com>
2025-08-05 00:39:40 +0000

    Updates from code review ; set default font, add padding of 10 to top of SVG and keep color consistent with assignment guidelines

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quizzia_splash_screen/onboarding.dart';
 import 'package:quizzia_splash_screen/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Onboarding());
+    return MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.ralewayTextTheme()),
+      home: Onboarding(),
+    );
   }
 }
